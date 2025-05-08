5c09affcf9e5c47682c5fd59bcb31319d3fd538e
badr2e <futxatpro@gmail.com>
2025-05-08 21:11:19 +0200

    r

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   env: {
     API_URL: process.env.API_URL || "http://localhost:5000/api",
   },
